bf9f4bdbdf931b8adeba65c31077a4af88926a72
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-12 01:31:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    console.log(`${musicians} plays ${instruments}`)
+  }
+  return theBeatlesPlay
+}
