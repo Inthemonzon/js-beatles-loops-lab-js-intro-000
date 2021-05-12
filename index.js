d04336264b085aabcb948242cf12f14bb5fccc91
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-12 01:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < musicians.length; i++) {
-    console.log(`${musicians} plays ${instruments}`)
+    array.push(musicians[i] + "plays" + instruments[i]);
   }
-  return theBeatlesPlay
+  return array
 }
